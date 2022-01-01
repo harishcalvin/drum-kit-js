c4ea3088430905ce5f911f2cd422d3976682d04c
harishcalvin <hari991996@gmail.com>
2022-01-01 12:12:16 +0530

    transition add

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 window.addEventListener('keydown', function(e) {
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     // console.log(audio)
     if (!audio) {
         return;
     }
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing')
 })
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(a => a.addEventListener('transitionend', removeTransition));
